cb8bf2921f417fddda405bfad05dfe55a12c0c3c
David Kong <dk.kong94@gmail.com>
2015-09-09 13:10:09 -0400

    added code to check the github secret.

---
+++ readme.md
@@ -0,0 +1,4 @@
+#ADS - Auto-Deployment-Server
+A simplified deployment server that pulls from a git repo and runs pm2 to deploy.
+
+In progress!
--- server.js
+++ server.js
@@ -1,1 +1,72 @@
 var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var crypto = require('crypto');
+
+var jwt = require('jsonwebtoken');
+var config = require('./config');
+
+
+// =======================
+// configuration =========
+// =======================
+//for crypto
+var algorithm = "SHA1"
+
+
+var port = process.env.PORT || 8080; // used to create, sign, and verify tokens
+app.set('githubSecret', config.secret); // secret variable
+// use body parser so we can get info from POST and/or URL parameters
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+app.use(bodyParser.json());
+
+// use morgan to log requests to the console
+app.use(morgan('dev'));
+
+// =======================
+// routes ================
+// =======================
+// basic route
+app.get('/', function (req, res) {
+	res.send('Hello! The API is at http://localhost:' + port + '/api');
+});
+app.get('/update/:repositoryName', function (req, res) {
+	var body = req.body;
+	var hmac = crypto.createHmac(algorithm, config.secret);
+	hmac.setEncoding('hex');
+	//check if the signature is correct.
+	hmac.end(body, function () {
+		hash = hmac.read();
+		if (req.header('X-Hub-Signature') === hash){
+			//the signature is correct. Carry on.
+			console.log("Signature verified");
+		}else{
+			res.error("Error, signautre doesn't check out.");
+		}
+	});
+});
+app.get('/update/nocheck'){
+	var body = req.body;
+	var hmac = crypto.createHmac(algorithm, config.secret);
+	hmac.setEncoding('hex');
+	//check if the signature is correct.
+	hmac.end(body, function () {
+		hash = hmac.read();
+		if (req.header('X-Hub-Signature') === hash){
+			//the signature is correct. Carry on.
+		}else{
+			res.error("Error, signature doesn't check out.");
+		}
+	});
+}
+function updateRepository(){
+	//TODO: Finish the code here.
+}
+// =======================
+// start the server ======
+// =======================
+app.listen(port);
+console.log('Magic happens at http://localhost:' + port);
